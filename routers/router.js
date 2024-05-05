4aee4e99d107e8df464e81fd815f74d7a4c5f28c
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-05 10:27:49 -0400

    instalacion de axios

--- routers/router.js
+++ routers/router.js
@@ -4,8 +4,24 @@ import path from 'path';
 const routers = express.Router();
 const __dirname = import.meta.dirname;
 
+//ruta raiz despliega la vista html
 routers.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+//ruta deportes lee y deslpiega en formato texto los deportes
+routers.get('/deportes', (req, res) => {
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../assets/data/data.json')));
+    res.send(data.deportes);
+});
+
+//ruta agregar recibe y agrega a la lista de datos el
+routers.get('/agregar', (req, res) => {
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../assets/data/data.json')));
+    const { nombre, precio } = req.body;
+    data.deportes.push({ nombre, precio });
+    fs.writeFileSync(path.join(__dirname, '../assets/data/data.json'), JSON.stringify(data));
+    res.send('ok');
 });
 
 export default routers;
